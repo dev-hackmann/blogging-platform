1078402e295dadca7842975dc62e59585def8f13
Hackmann <hackmann657@gmail.com>
2025-01-19 16:49:27 -0300

    Settings docker file and docker-compose

--- src/BloggingPlatform.Api/Program.cs
+++ src/BloggingPlatform.Api/Program.cs
@@ -16,8 +16,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    .Replace("${DB_HOST}", Environment.GetEnvironmentVariable("DB_HOST"))
+    .Replace("${DB_PORT}", Environment.GetEnvironmentVariable("DB_PORT"))
+    .Replace("${DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME"))
+    .Replace("${DB_USER}", Environment.GetEnvironmentVariable("DB_USER"))
+    .Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD"));
+
 builder.Services.AddDbContext<BloggingContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 builder.Services.AddScoped<IPostService, PostService>();
 
 var app = builder.Build();
@@ -27,9 +34,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
